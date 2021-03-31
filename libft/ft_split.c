3d6eb26d6fa1260f32c99e534527ee8147af9cb3
Kevin Davaine <kedavain@c2r12p1.42nice.fr>
2021-03-31 08:32:59 +0200

    Why ?

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,13 +6,29 @@
 /*   By: kedavain <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/29 13:20:21 by kedavain          #+#    #+#             */
-/*   Updated: 2021/03/29 13:44:51 by kedavain         ###   ########.fr       */
+/*   Updated: 2021/03/30 17:33:11 by kedavain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	charset(char *c, char *set)
+{
+	while (c)
+	{
+		if (c == set)
+			return (1);
+		c++;
+	}
+	return (0);
+}
+
 char	**ft_split(char const *s, char c)
 {
-	
+	char *scpy;
+	char *scpyresult
+
+	scpy = (char *)s;
+	while (charset(scpy, c))
+		scpy++;
 }
